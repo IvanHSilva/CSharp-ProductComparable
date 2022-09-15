523fb6bebbe256b3b80fb124848183c7be780ecf
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-15 12:58:44 -0300

    Class Product created

---
+++ ProductComparable/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProductComparable.Entities {
+    public class Product : IComparable<Product> {
+        // Attributes
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        // Constructor
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        // Methods
+        public override string ToString() {
+            return $"{Name}, {Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(Product other) {
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
