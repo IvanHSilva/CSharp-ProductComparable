94ede18ce7f59a5ab321842ba6648bb03bb3d7a9
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-15 12:54:57 -0300

    Add project files.

---
+++ ProductComparable/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductComparable {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
