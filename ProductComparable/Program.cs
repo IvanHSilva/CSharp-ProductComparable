b1fa0757d865e2f49a91c074dcd7cb7ee8292205
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-15 13:01:57 -0300

    Program created (version 1)

--- ProductComparable/Program.cs
+++ ProductComparable/Program.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using ProductComparable.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ProductComparable {
     internal class Program {
         static void Main(string[] args) {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV 4K", 1500.00));
+            list.Add(new Product("Notebok", 1200.00));
+            list.Add(new Product("Celular 128G", 2500.00));
+            list.Add(new Product("Sound Bar", 600.00));
+            list.Add(new Product("Pendrive 256G", 150.00));
+
+            list.Sort();
+
+            foreach (Product p in list) {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
         }
     }
 }
